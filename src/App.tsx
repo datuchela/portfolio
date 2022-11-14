b92bb2283c490024d8becf46df014b551d6324a7
datuchela <davit.narimanidze408@gmail.com>
2022-11-14 20:08:16 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Header />
+      <main>Hello</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="w-full p-2 bg-pink-200">datuchela</header>;
+};
+
+export default Header;
