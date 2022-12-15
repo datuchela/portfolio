291fa73a971fe94eea7de64e1b276839e10dc7b0
datuchela <davit.narimanidze408@gmail.com>
2022-12-15 16:14:50 +0400

    removed padding in y direction

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
-    <div data-theme={isDarkMode ? "dracula" : "light"} className="p-8 relative">
+    <div data-theme={isDarkMode ? "dracula" : "light"} className="px-8 relative">
       <ThemeButton themeState={{ isDarkMode, setIsDarkMode }} />
       <main className="flex flex-col">
         <Index isDarkMode={isDarkMode} />
